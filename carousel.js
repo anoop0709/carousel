8631665f7ba9986bc26a75f9a7f435be1048926a
Anoop <anoopsukumarannairk@gmail.com>
2022-03-27 15:10:39 +0100

    previous and next button working good,resize of the image good,dot indicator need to code

--- carousel.js
+++ carousel.js
@@ -1,25 +1,56 @@
+
 const track = document.querySelector('.carousel_track');
 const slides = Array.from(track.children);
 const nextButton = document.querySelector('.carousel_button--right');
 const preButton = document.querySelector('.carousel_button--left');
-const dotIndicator = Array.from(dotNav.children);
+
 const dotNav = document.querySelector('.carousel_nav');
+const dotIndicator = Array.from(dotNav.children);
+
+//calculate the width of each slide and positioned to left
 
 
-const slideSize = slides[0].getBoundingClientRect();
+const slideSize = slides[0].getBoundingClientRect().width;
+const setslidePosition = (slide,index) => {
+    slide.style.left = slideSize * index + 'px';
+};
+slides.forEach(setslidePosition);
 
-slides[0].style.left = slideSize * 0 + 'px';
-slides[1].style.left = slideSize * 1 + 'px';
-slides[2].style.left = slideSize * 2 + 'px';
-slides[3].style.left = slideSize * 3 + 'px';
-slides[4].style.left = slideSize * 4 + 'px';
+//function for moving the side left or right
+
+const moveToslide = (track,currentSlide,targetslide) => {
+    track.style.transform = 'translatex(-' + targetslide.style.left +')';
+    currentSlide.classList.remove('current_slide');
+    targetslide.classList.add('current_slide');
+}
 
 
 
 
 
 //when i click left,moves slides to the left
+
+preButton.addEventListener('click', e => {
+    const currentSlide = track.querySelector('.current_slide');
+    const prevSlide = currentSlide.previousElementSibling;
+    moveToslide(track,currentSlide,prevSlide);
+})
+
+
+
 //when i click right,moves slides to the right
 
+nextButton.addEventListener('click', e => {
+    const currentSlide = track.querySelector('.current_slide');
+    const nextSlide = currentSlide.nextElementSibling;
+    moveToslide(track,currentSlide,nextSlide);
+})
+
+
+
 //when i clicked the nav indicator its moves to that slide
-//
+
+dotNav.addEventListener('click', e => {
+    const targetdot = e.target.closest('button');
+    if(!targetdot) return;
+})
